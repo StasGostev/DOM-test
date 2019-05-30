0b620ed5569e8837cf4288e0f68f686b6d0f6d5e
StasGostev <spaceomen@gmail.com>
2019-05-30 17:43:43 +0300

    'Article-list_fixed'

--- article-list.js
+++ article-list.js
@@ -1,31 +1,53 @@
-
 'use strict';
 
 class ArticleList {
     constructor(container) {
         this.container = container;
+        this.tmpList = [];
     }
 
     addArticle(article) {
-        this.container.push(article);
+        this.tmpList.push(article);
     }
 
     removeArticle(article) {
-        this.container.shift()
+        let index = this.tmpList.indexOf(article);
+        this.tmpList.splice(index, 1);
     }
 
-    render() {
-        const list = document.querySelector('#article-list');
-        this.container.forEach(({title, author, text}) => {
-            const item = document.createElement('article');
-            const h1 =  document.createElement('h1').textContent = title;
-            const span =  document.createElement('span').textContent = author;
-            const p = document.createElement('p').textContent = text;
-            item.append(h1, span, p);
-            list.append(item);
-        })
-        this.container = [];
-        return list;
+    render(value) {
+        while(this.container.children.length !== 0) {
+            this.container.lastChild.remove();
+        }
+
+        if(value) {
+            let filtred = this.tmpList.filter((elem) => elem.matches(value));
+            filtred.forEach((elem) => this.container.append(makeDomElement(elem)))
+        } else {
+            this.tmpList.forEach((elem) => this.container.append(makeDomElement(elem)));
+        }
+
     }
 }
 
+
+function addRemoveButton() {
+    const btn = document.createElement('button');
+    btn.textContent = 'X';
+    btn.class = 'remover';
+    btn.style.backgroundColor = 'red';
+    return btn;
+}
+function makeDomElement(e){
+    const [title, author, text] = e;
+    const div =document.createElement('div');
+    const h1 = document.createElement('h1');
+    const span = document.createElement('span');
+    const p = document.createElement('p');
+    div.append(h1.textContent = title, 
+        span.textContent = author, 
+        p.textContent = text,
+        addRemoveButton());
+
+    return div;
+} 
